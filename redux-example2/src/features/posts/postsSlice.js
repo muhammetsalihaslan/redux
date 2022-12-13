89c0e619b20ade27160045c66a5cb83835be6e10
salih <muhammetsalihaslan@gmail.com>
2022-12-13 16:24:20 +0300

    add date-fns

--- redux-example2/src/features/posts/postsSlice.js
+++ redux-example2/src/features/posts/postsSlice.js
@@ -1,16 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: "1",
     title: "Learning Redux Toolkit",
     content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
   },
   {
     id: "2",
     title: "Slices...",
     content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
   },
 ];
 
@@ -28,6 +31,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId,
           },
         };
