d297f1c67404609bc661695754293eb5385373b6
salih <muhammetsalihaslan@gmail.com>
2022-12-21 11:50:25 +0300

    users part

--- redux-example2/src/features/users/usersSlice.js
+++ redux-example2/src/features/users/usersSlice.js
@@ -1,15 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-  { id: "0", name: "Dude Lobowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dvae Gray" },
-];
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
+
+// const initialState = [
+//   { id: "0", name: "Dude Lobowski" },
+//   { id: "1", name: "Neil Young" },
+//   { id: "2", name: "Dvae Gray" },
+// ];
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await axios.get(USERS_URL);
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
 });
 
 export const selectAllUsers = (state) => state.users;
